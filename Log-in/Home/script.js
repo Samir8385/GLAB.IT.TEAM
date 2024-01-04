9d69acfbc96bec0c2c72bfc47db871285fb93a31
“[Samir-Hacker]” <samirmc8384@gmail.com>
2024-01-04 16:59:12 +0600

    add all

--- Log-in/Home/script.js
+++
@@ -1,5 +0,0 @@
-function greet() {
-    var name = document.getElementById('username').value;
-    var string = "Username "+name;
-    document.getElementsByClassName('greet')[0].innerHTML = string;
-}
